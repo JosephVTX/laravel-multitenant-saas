c0b48d77f5799e4df77d8c260c6c773cd4a83240
JosephVTX <jvcallupe69@gmail.com>
2026-09-25 07:33:35 -0500

    Return API information JSON at the root instead of the Blade welcome page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return api_success([
+        'name' => config('app.name'),
+        'type' => 'REST API',
+        'version' => 'v1',
+        'base_url' => url('/api/v1'),
+        'health' => url('/api/v1/health'),
+        'documentation' => 'https://github.com/JosephVTX/laravel-multitenant-saas',
+    ], 'Multi-tenant SaaS API.');
 });
